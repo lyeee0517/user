0514f977feb93d32fb784122b4661d6095426ee5
lyeee0517 <lyeee0517@gmail.com>
2022-04-25 15:10:47 +0900

    두번째

--- test/src/main/java/com/user/UserDTO.java
+++ test/src/main/java/com/user/UserDTO.java
@@ -2,6 +2,8 @@ package com.user;
 
 public class UserDTO {
 	private String userId;
+	private String userName;
+	
 
 	public String getUserId() {
 		return userId;
@@ -9,6 +11,14 @@ public class UserDTO {
 
 	public void setUserId(String userId) {
 		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 	
 	
